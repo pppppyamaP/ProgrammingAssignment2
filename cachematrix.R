9d07a6993d07cbd951140fd36cd79abd904f9ec2
pppppyamaP <100580463+pppppyamaP@users.noreply.github.com>
2025-05-06 16:28:01 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# 関数 1: makeCacheMatrix (テンプレート)
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# 関数 2: cacheSolve (テンプレート)
+cacheSolve <- function(x, ...) { # 'x' は makeCacheMatrix が返したリスト
+    inv <- x$getInverse()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mat_data <- x$get()
+    inv <- solve(mat_data, ...) # solve() で逆行列を計算
+    x$setInverse(inv)
+    inv
 }
